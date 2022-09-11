45a80267499141b20374cbdd1874d0ceeebfc299
Evgeny <angst_eam@mail.ru>
2022-09-11 16:59:02 +0300

    Initial commit

---
+++ src/test/java/ru/training/animalfacts/AnimalFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.training.animalfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
